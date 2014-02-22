61547b8b5042fa887457efb97bfc08900f1c6312
cmendoza <cmendoza@chopper>
2014-02-22 16:13:10 +0100

    eliminando archivos innecesarios

--- impressive-themed-categories.php
+++ impressive-themed-categories.php
@@ -8,6 +8,7 @@
   Author Email: cmendoza@opensistemas.com, tosona@opensistemas.com
  */
 if (!class_exists('ITC_Plugin')) {
+
     class ITC_Plugin {
 
         static $uniqueObject = null;
@@ -16,35 +17,26 @@ if (!class_exists('ITC_Plugin')) {
         var $rutaIconos;
         var $tablename;
 
-        static function & getInstance() {
-
-            if (null == ITC_Plugin::$uniqueObject) {
-                ITC_Plugin::$uniqueObject = new ITC_Plugin();
-            }
-
-            return ITC_Plugin::$uniqueObject;
-        }
-
         function __construct() {
             global $wpdb;
             add_action('wp_ajax_fancypost_remove_icon', array(&$this, 'ajax_remove_icon'));
             add_action('admin_enqueue_scripts', array(&$this, 'incluirJs'));
             add_action('plugins_loaded', array(&$this, 'load_text_domain'));
-            
+
             //Form para categorias
             add_action('category_add_form_fields', array($this, 'itc_add_category_fields'));
             add_action('category_edit_form', array($this, 'itc_edit_category_fields'));
-            
+
             //acciones para CRUD en categorias
             add_action('create_category', array(&$this, 'itc_save'), 10, 2);
             add_action('edit_category', array(&$this, 'itc_save'), 10, 2);
             add_action('delete_category', array($this, 'itc_delete'));
-            
+
             add_action('admin_menu', array(&$this, 'add_option_menu'));
-            add_action('admin_init', array(&$this,'register_and_build_fields'));
-            
+            add_action('admin_init', array(&$this, 'register_and_build_fields'));
+
             $this->rutaIconos = content_url("themes/" . get_template() . $this->icons_path);
-            $this->tablename = $wpdb->prefix."itc";
+            $this->tablename = $wpdb->prefix . "itc";
         }
 
         function instalar() {
@@ -68,116 +60,68 @@ if (!class_exists('ITC_Plugin')) {
             $plugin_dir = basename(dirname(__FILE__));
             load_plugin_textdomain('itc', false, $plugin_dir . "/languages");
         }
-        
-        function add_option_menu(){
-            //add_posts_page(__('Catalog Options', 'catalogo'), __('Catalog Options', 'catalogo'), 'edit_posts', 'catalog-options', array(&$this, 'show_catalog_options'));
+
+        function add_option_menu() {
             add_options_page(__('IT Categories', 'itc'), __('IT Categories', 'itc'), 'manage_options', 'itc-options', array(&$this, 'show_itc_options'));
-            //add_options_page($page_title, $menu_title, $capability, $menu_slug, $function);
         }
-        
-        function show_itc_options(){
+
+        function show_itc_options() {
             ?><div id="itc-options-wrap" class="wrap">
-                <h2><?php _e('Impressive Themed Categories Options', 'itc');?></h2>
-                <!--<div id="tabs-itc">
-                    <ul>
-                        <li><a href="#tabs-settings"><?php _e("Settings", "itc");?></a></li>
-                        <li><a href="#tabs-categories"><?php _e("Categories")?></a></li>
-                    </ul>
-                    <div id="tabs-settings">-->
-                        <form action="options.php" method="post">
-                            <?php 
-                            settings_fields('itc_options');
-                            do_settings_fields('itc_options', 'itc_colors');
-                            do_settings_sections(__FILE__);
-                            submit_button();
-                            ?>
-                        </form>
-                        <?php include "itc-icono-plantilla.php"; ?>
-                    <!--</div>
-                    <div id="tabs-categories">
-                        <form>
-                        <?php
-                         wp_dropdown_categories(array("hide_empty" => 0));
-                         include "itc-plantilla.php";
-                         submit_button();
-                        ?>
-                        </form>
-                    </div>
-                </div>-->
+                <h2><?php _e('Impressive Themed Categories Options', 'itc'); ?></h2>
+                <form action="options.php" method="post">
+                    <?php
+                    settings_fields('itc_options');
+                    do_settings_fields('itc_options', 'itc_colors');
+                    do_settings_sections(__FILE__);
+                    submit_button();
+                    ?>
+                </form>
+            <?php include "itc-icono-plantilla.php"; ?>
             </div>
-            <!--<script type="text/javascript">
-            jQuery("document").ready(function() {
-                jQuery( "#tabs-itc" ).tabs();
-            });
-            </script>--><?php
+            <?php
         }
-        
-        function register_and_build_fields(){
+
+        function register_and_build_fields() {
             register_setting('itc_options', 'itc_colors', array(&$this, 'validate_colors'));
             add_settings_section('color_section', __('Color Settings', 'itc'), array(&$this, 'section_cb'), __FILE__);
-            add_settings_field('itc_colors', __("Colors:", "itc"), array(&$this, 'itc_colors_cb'), __FILE__, 'color_section' );
+            add_settings_field('itc_colors', __("Colors:", "itc"), array(&$this, 'itc_colors_cb'), __FILE__, 'color_section');
         }
-        
-        function section_cb(){}
-        
-        function validate_colors($value){
-            return json_decode($value,true);
+
+        function section_cb() {
+            
         }
-        
-        function validate_setting($value){
+
+        function validate_colors($value) {
+            return json_decode($value, true);
+        }
+
+        function validate_setting($value) {
             return $value;
         }
-        
-        function itc_colors_cb(){
+
+        function itc_colors_cb() {
             include "itc-color-plantilla.php";
         }
-        
-        function itc_icons_cb(){
+
+        function itc_icons_cb() {
             include "itc-icono-plantilla.php";
         }
 
         function incluirJs($hook) {
-            global $post;
-            $arrScript = array(
-                "errorNoName"                 => __("Missing Catalog Name", "itc")
-                , "errorNameInUse"              => __("The name is already in use", "itc")
-                , "errorNoMatchesFound"         => __("No matches found", "itc")
-                , "errorSearch"                 => __("Wrong search term", "itc")
-                , "warningRemoveIcon"           => __("This action can not be undone. Are you sure you want to proceed?", "itc")
-            );
+            wp_enqueue_script('itc_script', plugins_url(basename(__DIR__) . '/js/itc.js'));
+            wp_enqueue_script('color-script-jquery', plugins_url(basename(__DIR__) . "/js/simplecolorpicker/jquery.simplecolorpicker.js"), array('jquery'));
+            wp_enqueue_style('color-script-css', plugins_url(basename(__DIR__) . "/js/simplecolorpicker/jquery.simplecolorpicker.css"));
+            wp_enqueue_script('image-picker', plugins_url(basename(__DIR__) . "/js/image-picker/image-picker.min.js"), array('jquery'));
+            wp_enqueue_style('image-picker-css', plugins_url(basename(__DIR__) . "/js/image-picker/image-picker.css"));
 
-            wp_enqueue_script('itc_script', plugins_url(basename(__DIR__).'/js/itc.js'));
-            wp_localize_script('itc_script', 'objectL10n', $arrScript);
-            wp_enqueue_script('jquery-select2', plugins_url(basename(__DIR__)."/js/select2-3.4.0/select2.js"));
-            wp_enqueue_style('cap-jquery-select2-css', plugins_url(basename(__DIR__)."/js/select2-3.4.0/select2.css"));
-            wp_enqueue_script('color-script-jquery', plugins_url(basename(__DIR__)."/js/simplecolorpicker/jquery.simplecolorpicker.js"), array('jquery'));
-            wp_enqueue_style('color-script-css', plugins_url(basename(__DIR__)."/js/simplecolorpicker/jquery.simplecolorpicker.css"));
-            wp_enqueue_script('image-picker', plugins_url(basename(__DIR__)."/js/image-picker/image-picker.min.js"), array('jquery'));
-            wp_enqueue_style('image-picker-css', plugins_url(basename(__DIR__)."/js/image-picker/image-picker.css"));
-            
-            if($hook == "settings_page_itc-options"){
+            if ($hook == "settings_page_itc-options") {
                 wp_enqueue_script('jquery-ui-draggable');
-                wp_enqueue_script('jquery-ui-tabs');
                 wp_enqueue_script('iris');
-                wp_enqueue_script('dnd-fileupload', plugins_url(basename(__DIR__)."/js/jquery-fileupload/jquery.fileupload.js"));
-                wp_enqueue_script('dnd-fileupload-ui', plugins_url(basename(__DIR__)."/js/jquery-fileupload/jquery.fileupload-ui.js"));
-                wp_enqueue_style('dnd-fileupload-style', plugins_url(basename(__DIR__)."/js/jquery-fileupload/jquery.fileupload-ui.css"));
-                wp_enqueue_style('jquery-ui-tabs-style', plugins_url(basename(__DIR__)."/css/jquery-ui-1.10.4.custom.css"));
-                wp_enqueue_script('itc-iconupload', plugins_url(basename(__DIR__)."/js/iconupload.js"), array('plupload', 'plupload-html5', 'plupload-html4', 'plupload-handlers'));
+                wp_enqueue_script('dnd-fileupload', plugins_url(basename(__DIR__) . "/js/jquery-fileupload/jquery.fileupload.js"));
+                wp_enqueue_script('dnd-fileupload-ui', plugins_url(basename(__DIR__) . "/js/jquery-fileupload/jquery.fileupload-ui.js"));
+                wp_enqueue_style('dnd-fileupload-style', plugins_url(basename(__DIR__) . "/js/jquery-fileupload/jquery.fileupload-ui.css"));
+                wp_enqueue_script('itc-iconupload', plugins_url(basename(__DIR__) . "/js/iconupload.js"), array('plupload', 'plupload-html5', 'plupload-html4', 'plupload-handlers'));
             }
-        }
-
-        function fancyPostBox() {
-            add_meta_box(
-                    'itc_info'
-                    , __('ITC Information', 'itc')
-                    , array(&$this, 'inner_itc_info')
-            );
-        }
-
-        function inner_itc_info($post) {
-            //print_r($catalogo);
-            include (dirname(__FILE__) . "/itc-plantilla.php");
         }
 
         function getAvailableIcons() {
@@ -192,74 +136,69 @@ if (!class_exists('ITC_Plugin')) {
             return $availableIcons;
         }
 
-        function getColors(){
+        function getColors() {
             return get_option('itc_colors');
         }
-        
-        function getIcons(){
-            return get_option('itc_icons');
-        }
-        
+
         function restrictIconUploadSize() {
             return 2097152;
         }
-                
-        function restrictIconTypes($plupload_init){
-            $plupload_init["url"] = plugins_url(basename(__DIR__)."/dnd-upload.php");
+
+        function restrictIconTypes($plupload_init) {
+            $plupload_init["url"] = plugins_url(basename(__DIR__) . "/dnd-upload.php");
             $plupload_init["filters"][0]["extensions"] = "jpg,jpeg,png";
             $plupload_init["max_file_size"] = $this->restrictIconUploadSize();
             return $plupload_init;
         }
-        
+
         function ajax_remove_icon() {
             $icon = filter_input(INPUT_POST, "icono");
             @unlink(get_template_directory() . $this->icons_path . $icon);
             die();
         }
-        
-        function itc_add_category_fields(){
-            ?><h3><?php _e("ITC Settings", "itc");?></h3><?php
+
+        function itc_add_category_fields() {
+            ?><h3><?php _e("ITC Settings", "itc"); ?></h3><?php
             include "itc-plantilla-categories.php";
         }
-        
-        function itc_edit_category_fields($term_obj){
+
+        function itc_edit_category_fields($term_obj) {
             $itc = $this->getITC($term_obj->term_id);
             ?><h3><?php _e("ITC Settings", "itc"); ?></h3><?php
             include "itc-plantilla-categories-edit.php";
         }
-        
-        function itc_delete($term_id){
+
+        function itc_delete($term_id) {
             /* @var $wpdb wpdb */
             global $wpdb;
             $wpdb->delete($this->tablename, array("category_id" => $term_id), array("%d"));
-            
         }
-        
-        function itc_save($term_id){
+
+        function itc_save($term_id) {
             global $wpdb;
             $color = filter_input(INPUT_POST, "category_color");
             $icon = filter_input(INPUT_POST, "category_icon");
             $icon = str_replace($this->rutaIconos, "", $icon);
-            
-            if(count($this->getITC($term_id))){
-                $wpdb->update($this->tablename, array("color" => $color, "icon" => $icon) , array("category_id" => $term_id), array("%s", "%s"), array("%d"));
-            }else{
+
+            if (count($this->getITC($term_id))) {
+                $wpdb->update($this->tablename, array("color" => $color, "icon" => $icon), array("category_id" => $term_id), array("%s", "%s"), array("%d"));
+            } else {
                 $wpdb->insert($this->tablename, array("category_id" => $term_id, "color" => $color, "icon" => $icon), array("%d", "%s", "%s"));
             }
         }
-        
-        function getITC($cat_id){
+
+        function getITC($cat_id) {
             /* @var $wpdb wpdb */
             global $wpdb;
             $vals = $wpdb->get_row($wpdb->prepare("SELECT color, icon FROM $this->tablename WHERE category_id = %s", intval($cat_id)), ARRAY_A);
-            if(is_null($vals)){
+            if (is_null($vals)) {
                 return array();
-            }else {
-                $vals["icon"] = $this->rutaIconos.$vals['icon'];
+            } else {
+                $vals["icon"] = $this->rutaIconos . $vals['icon'];
                 return $vals;
             }
         }
-        
+
     }
 
     $oITC = new ITC_Plugin();
